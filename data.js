298a03f42308202d929051c1d2a922ae09eeb768
Joao Victor <joaovictorcostasouza15@gmail.com>
2020-08-06 15:42:29 -0300

    Dado dos videos

---
+++ data.js
@@ -0,0 +1,43 @@
+module.exports=[
+
+    {
+        id:"eJ0YaLnxB0c",
+        title:"Terror Subestimados",
+        time:"10 min",
+        price:"Free",
+        feature: true
+    },
+
+    {
+        id:"-BILNg9RV_w",
+        title:"Suspenses Subestimados",
+        time:"10 min",
+        price:"Free",
+        feature: true
+    },
+    {
+        id:"t_vkKrsRwAw",
+        title:"32 erros em coringa",
+        time:"10 min",
+        price:"Free",
+        feature: true
+    },
+    {
+        id:"AJbNGitMVpc",
+        title:"É bom: Anaconda",
+        time:"10 min",
+        price:"Free"
+    },
+    {
+        id:"MXGaMM0Chy0",
+        title:"Depois eu arrumo ",
+        time:"10 min",
+        price:"Free"
+    },
+    {
+        id:"HumH_iVe71c",
+        title:"Sei la cansei",
+        time:"10 min",
+        price:"Free"
+    }
+]
